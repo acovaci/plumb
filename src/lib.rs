fdb271ce8050d81f3bc27c2d98f594ad240c5535
Adrian Covaci <6562353+acovaci@users.noreply.github.com>
2024-05-19 14:20:52 +0100

    feat(cli): Implemented plumb version command

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
-pub fn hello() -> String {
-    "Hello, world!".to_string()
+pub mod version;
+
+pub fn command_version() -> String {
+    let version = version::version();
+    format!("plumb {}", version)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_version() {
+        assert!(command_version().contains("plumb"));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-use plumb::hello;
+use clap::{Parser, Subcommand};
+use plumb::command_version;
+
+#[derive(Parser, Debug)]
+#[command(name = "plumb", version)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Version,
+}
 
 pub fn main() {
-    println!("{}", hello());
+    let cli = Args::parse();
+
+    match cli.command {
+        Some(Command::Version) => {
+            command_version();
+        }
+        _ => {
+            println!("Invalid arguments. Try `plumb help` for more information.");
+        }
+    }
 }
